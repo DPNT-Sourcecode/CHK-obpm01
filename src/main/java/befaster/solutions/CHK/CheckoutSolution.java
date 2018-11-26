e78659e76fcff70aabc814afea5773beed34ad6c
sbx_user1059 <sbx_user1059@ip-10-128-12-122.eu-west-2.compute.internal>
2018-11-26 01:59:29 +0000

    Mon Nov 26 01:59:29 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,9 +6,9 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
 
-	HashMap<Character, Integer> charFequencyMap = new HashMap<Character, Integer>();
 
 	public Integer checkout(String skus) {
+	 HashMap<Character, Integer> charFequencyMap = new HashMap<Character, Integer>();
 		for (char c : skus.toCharArray()) {
 			Integer value = charFequencyMap.get(c);
 			if (value != null) {
@@ -49,15 +49,40 @@ public class CheckoutSolution {
 //				break;
 			}
 		}
+		System.out.println(skus + " >>>> " + sum);
 		return sum;
 	}
 
 
 	
 
-//	public static void main(String[] args) {
-//		CheckoutSolution cks = new CheckoutSolution();
-//		cks.checkout("AAAABC");
-//		System.out.println("<>>>>");
-//	}
+	public static void main(String[] args) {
+		CheckoutSolution cks = new CheckoutSolution();
+		cks.checkout("AAAABC");
+		cks.checkout("");//, resp = 0
+		cks.checkout("A");//, resp = 50
+		cks.checkout("B");//, resp = 80
+		cks.checkout("C");//, resp = 100
+		cks.checkout("D");//, resp = 115
+		cks.checkout("a");//, resp = -1
+		cks. checkout("-");//, resp = -1
+		cks.checkout("ABCa");//, resp = -1
+		cks.checkout("AxA");//, resp = -1
+		cks.checkout("ABCD");//, resp = -1
+		cks.checkout("A");//, resp = -1
+		cks.checkout("AA");//, resp = -1
+		cks.checkout("AAA");//, resp = -1
+		cks.checkout("AAAA");//, resp = -1
+		cks.checkout("AAAAA");//, resp = -1
+		cks. checkout("AAAAAA");//, resp = -1
+		cks. checkout("B");//, resp = -1
+		cks. checkout("BB");//, resp = -1
+		cks. checkout("BBB");//, resp = -1
+		cks.checkout("BBBB");//, resp = -1
+		cks. checkout("ABCDABCD");//, resp = -1
+		cks. checkout("BABDDCAC");//, resp = -1
+		cks.checkout("AAABB");//, resp = -1
+		cks.checkout("ABCDCBAABCABBAAA");//, resp = -1
+		System.out.println("<>>>>");
+	}
 }
