567591b84900b9681c3e03614ac1f74066023bb1
sbx_user1059 <sbx_user1059@ip-10-128-12-122.eu-west-2.compute.internal>
2018-11-26 00:22:46 +0000

    Mon Nov 26 00:22:46 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,5 +1,22 @@
 package befaster.solutions.HLO;
 
-public class HelloSolutionTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import junit.framework.Assert;
+
+public class HelloSolutionTest {
+	private HelloSolution hello;
+	
+	@Before
+	public void setUp(){
+		hello = new HelloSolution();
+	}
+	
+	@Test
+	public void checkHello(){
+//		assertThat(hello.hello("Foo"), equals("Hello Foo"));
+		Assert.assertEquals("Test failed", "Hello Foo", hello.hello("Foo"));
+		
+	}
 }
