077768d08fecfd4ed519c3327674648b44bbcb80
sbx_user1059 <sbx_user1059@ip-10-128-12-122.eu-west-2.compute.internal>
2018-11-26 00:48:41 +0000

    Mon Nov 26 00:48:41 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,37 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+
+	HashMap<Character, Integer> charFequencyMap = new HashMap<Character, Integer>();
+
+	public Integer checkout(String skus) {        
+		generateSKFrequence(skus);
+		calculatePrizeTotal();
+		return null;
     }
+	
+	private void generateSKFrequence(String skus){
+		for(char c : skus.toCharArray()){
+    		Integer value = charFequencyMap.get(c);
+    		if(value != null){
+    			charFequencyMap.put(c, new Integer(value + 1));
+    		}else{
+    			charFequencyMap.put(c, 1);
+    		}
+    	}
+	}
+	
+	private void calculatePrizeTotal(){
+		
+	}
+	
+	public static void main (String[] args){
+		CheckoutSolution cks = new CheckoutSolution();
+		cks.generateSKFrequence("AAAABC");
+		System.out.println("<>>>>");
+	}
 }
