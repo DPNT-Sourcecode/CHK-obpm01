630ca99a4b99823959bbf3c4357ef722d262d95f
sbx_user1059 <sbx_user1059@ip-10-128-12-122.eu-west-2.compute.internal>
2018-11-26 01:09:42 +0000

    Mon Nov 26 01:09:42 UTC 2018

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,5 +1,22 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class CheckoutSolutionTest {
 
+	private CheckoutSolution checkout;
+	
+	@Before
+	public void setUp(){
+		checkout = new CheckoutSolution();
+		
+	}
+	
+	@Test
+	public void testChecout(){
+		Assert.assertEquals(expected, actual);
+	}
 }
